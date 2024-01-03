e61e89131b1f3bd51ab9d175d61567dc124f6539
Benjamin Appiah <Shell Script to run Python script>
2024-01-03 14:10:37 -0800

    print ASCII alphabets in lowercae in python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = str(number)[-1]
+_string = "Last digit of"
 if int(last_digit) > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"{_string} {number} is {last_digit} and is greater than 5")
 elif last_digit == '0':
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif number < 0:
-    print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+    print(f"{_string} {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"{_string} {number} is {last_digit} and is less than 6 and not 0")
