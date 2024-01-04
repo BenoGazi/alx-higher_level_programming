a1a4a99724be11b3dbea2f73b00c3b492d3dca0c
Benjamin Appiah <Shell Script to run Python script>
2024-01-04 12:25:41 -0800

    Hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     _hid = dir()
     for i in range(0, len(_hid)):
         if _hid[i][:2] != "__":
-            print("{:s}".format(_hid(i)))
+            print("{:s}".format(_hid[i]))
