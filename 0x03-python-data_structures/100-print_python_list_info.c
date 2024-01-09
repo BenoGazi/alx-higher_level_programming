e9a6b6a5e6fc4de90f6a62e256599f1e98950c76
Benjamin Appiah <Shell Script to run Python script>
2024-01-09 09:57:22 -0800

    Let's try again

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,14 +6,14 @@ void print_python_list_info(PyObject *p)
 {
 	long int size = PyList_Size(p);
 	int i;
-	PyListobject *_obj = (PyListObject *)p;
+	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated %li\n", obj->allocated);
+	printf("[*] Allocated =  %li\n", obj->allocated);
 	i = 0;
 	while (i < size)
 	{
-		printf("Element %i: %s\n", i, Py_TYPE(_obj->ob_item[i]->tp_name));
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 		i++;
 	}
 }
