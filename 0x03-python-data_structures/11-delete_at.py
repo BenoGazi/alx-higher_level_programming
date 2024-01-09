7625b5f48bda349b5c15d323e4163c8bc30133e2
Benjamin Appiah <Shell Script to run Python script>
2024-01-08 21:52:20 -0800

    Corrections made for delete at task

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    _len = len(my_list)
-    if idx > _len -1:
+    length = len(my_list)
+    if idx > length -1:
         return my_list
     elif idx < 0:
         return my_list
