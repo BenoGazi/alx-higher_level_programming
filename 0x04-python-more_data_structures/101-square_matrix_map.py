f3b4a29a1593e4cde25f2c2cada615b33c45efe2
Benjamin Appiah <Shell Script to run Python script>
2024-01-11 18:31:42 -0800

    Corrections

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda horizontal: list(map(lambda vertical: vertical ** 2, horizontal)), matrix))
+    return list(map(lambda h: list(map(lambda v: v ** 2, h)), matrix))
