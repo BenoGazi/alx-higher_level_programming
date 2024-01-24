e739749a127101fb82a97398217acd52575210ec
Benjamin Appiah <Shell Script to run Python script>
2024-01-23 17:39:34 -0800

    HMM python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,18 +2,18 @@
 
 """Definition of Square class."""
 
-class: Square:
+class Square:
     """Square class."""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """New Square
 
         args:
             size (int: New square size).
         """
-        if not instance(size, int):
+        if not isinstance(size, int):
             raise TypeError("Size must be an integer")
         elif size < 0:
-            raise ValueError("size must greater than 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
