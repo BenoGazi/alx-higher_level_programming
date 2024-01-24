6add72ac3e1fcc1ca0dd1140286cacff278b83d2
Benjamin Appiah <Shell Script to run Python script>
2024-01-23 17:42:31 -0800

    hmm

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """New Square
 
-        args:
+        Args:
             size (int: New square size).
         """
         if not isinstance(size, int):
