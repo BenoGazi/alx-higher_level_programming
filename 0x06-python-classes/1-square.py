0806f7795dfba08dc80ede3cc947da30c0a96671
Benjamin Appiah <Shell Script to run Python script>
2024-01-23 17:20:45 -0800

    File 1 added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define a square class."""
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        """New Square.
+        Args:
+        size (int): The size of the new square."""
+        self.__size = size
