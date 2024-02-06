5392cf556e03dab4c6bfbd9518a0f6ebf16c6d56
Benjamin Appiah <Shell Script to run Python script>
2024-02-06 13:20:39 -0800

    Covert to Json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns a JSON representation of an object string"""
+import json
+def to_json_string(my_obj):
+    """JSON representation of the string object"""
+    return json.dumps(my_obj)
+
