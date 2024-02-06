68578e453a19c20844b51f59cc929e8c7bf081b4
Benjamin Appiah <Shell Script to run Python script>
2024-02-06 15:22:57 -0800

    hmm

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Python class to Json function"""
 def class_to_json(obj):
-"""Return the dctionary of a simple data structure"""
+"""Return the dictionary of a simple data structure"""
     return obj.__dict__
