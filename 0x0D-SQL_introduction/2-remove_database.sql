f81e6721cc8782298d193eb44f042789c1dc579c
Benjamin Appiah <Shell Script to run Python script>
2024-03-13 11:10:31 -0700

    Let's get this done

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
--- Removes DataBase hbtn_0c_0
--- If the database doesn't exits, the script should fail
-DROP DATABASE IF EXITS 'hbtn_0c_0';
+-- Removes DataBase hbtn_0c_0.
+-- If the database doesn't exits, the script should fail.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
