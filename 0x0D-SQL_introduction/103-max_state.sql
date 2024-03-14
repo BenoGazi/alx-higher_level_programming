e1219e1d86a3843d986f85300ce3df3795f5a8f7
Benjamin Appiah <Shell Script to run Python script>
2024-03-13 19:40:27 -0700

    Letts try again

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
--- Show the max temperatire of each state
+-- Show the max temperature of each state
 -- Ordered by state name
 SELECT `state`, MAX(`value`) AS `max_temp`
-FROM `temperature`
+FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
