77eb128759f15b507f910b1f2d1afd21affeccb4
Benjamin Appiah <Shell Script to run Python script>
2024-03-16 05:33:53 -0700

    Taks 3 addd

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 --Create the user 'user_0d_2'
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant permisions to user on the hbtn_0d_2 database
-GRANT SELECTION ON hbtn_0d_2.* TO user_0d_2@localhost
+GRANT SELECTION ON hbtn_0d_2.* TO user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Creates a table 'force_name' on my MYSQL server
+-- Create a table in the database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
