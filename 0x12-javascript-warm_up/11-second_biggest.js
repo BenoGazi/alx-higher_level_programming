16a00cf7aae8832c9d4c90c7c13553225a348abf
BenoGazi <ben.appiah451@gmail.com>
2024-04-11 22:05:28 -0700

    Lets check again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const sec_arr = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(sec_arr);
+    const arr = process.argv.slice(2).map(Number);
+    const secarr = arr.sort(function (a, b) { return b - a; })[1];
+    console.log(secarr);
 }
