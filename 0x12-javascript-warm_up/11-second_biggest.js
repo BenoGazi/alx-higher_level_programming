2e6368c325337f45d7ea4ed624ed5ea70c01241f
BenoGazi <ben.appiah451@gmail.com>
2024-04-11 22:11:13 -0700

    Lets gooo

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-    const arr = process.argv.slice(2).map(Number);
-    const secarr = arr.sort(function (a, b) { return b - a; })[1];
-    console.log(secarr);
+  const arr = process.argv.slice(2).map(Number);
+  const secarr = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(secarr);
 }
