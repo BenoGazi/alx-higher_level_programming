fe29a16492ef84874b20c9b592803504d196e2fd
BenoGazi <ben.appiah451@gmail.com>
2024-04-11 17:53:05 -0700

    Modified

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-	console.log('0');
+  console.log('0');
 } else {
-	const arr = process.argv.slice(2).map(Number);
-	const sec_arr = arr.sort(function (a, b) { return b - a; })[1];
-	console.log(sec_arr);
+  const arr = process.argv.slice(2).map(Number);
+  const sec_arr = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(sec_arr);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-	console.log('Missing size')
+  console.log('Missing size');
 } else {
-	const x = Number(process.argv[2]);
-	let i = 0;
-	while (i < x) {
-		console.log('X'.repeat(x));
-	}
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
