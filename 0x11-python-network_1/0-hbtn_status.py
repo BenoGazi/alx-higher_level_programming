31e0bd84487baf9e0a561b6a56e3ebe454da96b1
BenoGazi <ben.appiah451@gmail.com>
2024-05-01 06:43:47 +0000

    First check

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+#Fetches https://alx-intranet.hbtn.io/status
+if __name__ == "__main__":
+    import urllib.request
+    data = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(data) as response:
+        print ("Body response:")
+        print ("\t- type: {}".format(type(response.read())))
+        response.seek(0)
+        print("\t- content: {}".format(response.read()))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
