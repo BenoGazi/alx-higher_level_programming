357b4e3e17d0e8534cfef342b08abe8d9f8029bd
BenoGazi <ben.appiah451@gmail.com>
2024-05-04 02:43:03 +0000

    Lets check out code

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email and sends a POST request"""
+if __name_ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    url = sys.argv[1]
+    e_mail = sys.argv[2]
+    pl = {'email': e_mail}
+    pl = urllib.parse.urlencode(pl)
+    pl = pl.encode('ascii')
+    req = urllib.request.Request(url, pl)
+    with urllib.request.urlopen(req) as res:
+        print(response.read().decode('utf-8'))
+
